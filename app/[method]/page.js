00569784e19c03a41f490a3b383b8090d248cdf3
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-04-03 19:22:51 +0500

    second commit

--- app/[method]/page.js
+++ app/[method]/page.js
@@ -1,21 +1,87 @@
-/* eslint-disable */
 
 
 
-import requests from "../../utils/requests";
+
+
 import Results from "../../components/Results/Results"
 
 
 
+
+
+
+
 export async function  getMoviesData(para) {
-        /* eslint-disable */
-        const url=`https://api.themoviedb.org/3${requests[para].url || ''}` 
-        /* eslint-enable */
-        const res = await fetch(url, {next: {revalidate: 60}})
-        const data= await res.json()
-      
-        return data.results;
-   
+
+
+
+
+  
+  
+    if (para === 'fetchPopular') {
+
+      const url=`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if (para === 'fetchTrending') {
+
+      const url=`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}&language=en=us`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchNetFlixOriginals") {
+
+      const url=`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchTopRated") {
+
+      const url=`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchAction") {
+
+      const url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=28`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchComedy") {
+
+      const url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=35`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchDocumentaries") {
+
+      const url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=99`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchRomance") {
+
+      const url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=10749`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+    else if(para === "fetchWar") {
+
+      const url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=10752`
+      const res = await fetch(url, {next: {revalidate: 60}})
+      const data= await res.json()
+      return data.results;
+    }
+
 
 }
 
@@ -28,7 +94,7 @@ const page = async ({params}) => {
 
   return (
     <div className="moviescontainer flex  w-full  flex-wrap items-center justify-center gap-y-8 gap-x-4 ">
-            {moviesData.map(movie=> 
+            {moviesData?.map(movie=> 
               <Results
                id={movie.id} 
               posterPath={movie.poster_path} 
@@ -50,11 +116,11 @@ export default page
 
 
 
-export async function generateStaticParams() {
+// export async function generateStaticParams() {
 
-  const arr=['fetchPopular', 'fetchTrending', 'fetchNetFlixOriginals', 'fetchTopRated', 'fetchAction', 'fetchComedy', 'fetchHorror', 'fetchDocumentaries', 'fetchRomance', 'fetchWar']
-  return arr.map(d=>{
-    method: d.toString()
-  })
+//   const arr=['fetchPopular', 'fetchTrending', 'fetchNetFlixOriginals', 'fetchTopRated', 'fetchAction', 'fetchComedy', 'fetchHorror', 'fetchDocumentaries', 'fetchRomance', 'fetchWar']
+//   return arr.map(d=>{
+//     method: d.toString()
+//   })
 
-}
+// }
--- utils/requests.js
+++ utils/requests.js
@@ -1,48 +1,49 @@
-const API_KEY= "9fe83855ebd3956603f14fe651eb4a00";
 
-export default   {
+/* eslint-disable */
+
+export  default  {
     fetchPopular:{
         title: "Popular",
-        url: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        url: `/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
     },
     fetchTrending:{
         title:'Trending',
-        url:`/trending/all/week?api_key=${API_KEY}&language=en=us`
+        url:`/trending/all/week?api_key=${process.env.API_KEY}&language=en=us`
     },
     fetchNetFlixOriginals:{
         title:'NetFlixOriginals',
-        url:`/discover/tv?api_key=${API_KEY}&with_networks=213`
+        url:`/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`
     },
     fetchTopRated: {
         title:'TopRated',
-        url:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+        url:`/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
     },
     fetchAction:{
         title:'Action',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=28`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=28`
     },
     fetchComedy:{
         title:'Comedy',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=35`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=35`
     },
     fetchHorror:{
         title:'Horror',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=27`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=27`
     },
     fetchDocumentaries:{
         title:'Documentaries',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=99`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=99`
     },
 
     fetchRomance:{
         title:'Romance',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=10749`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=10749`
     },
     fetchWar:{
         title:'War',
-        url:`/discover/movie?api_key=${API_KEY}&with_genres=10752`
+        url:`/discover/movie?api_key=${process.env.API_KEY}&with_genres=10752`
     },
-    APIKEY: API_KEY
+    APIKEY: process.env.API_KEY
 
 }
 
